fd967db47963ec78d064fe7dc56408a4db432bef
Emmanuel Salazar <emmanuel.salazar@avantica.net>
2022-04-25 20:20:09 -0600

    Agregar archivos de proyecto.

---
+++ SeleniumFirstProject/SimpleApplicationRunner.cs
@@ -0,0 +1,19 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using WebDriverManager;
+using WebDriverManager.DriverConfigs.Impl;
+
+namespace SelenmiumFirstProject
+{
+    public class SimpleApplicationRunner
+    {
+        public static void Main(string[] args)
+        {
+            new DriverManager().SetUpDriver(new ChromeConfig());
+
+            IWebDriver driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl("https://github.com");
+        }
+    }
+}
