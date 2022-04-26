e9115332ca6056893c809450ff4dab5b1cae4187
Emmanuel Salazar <emmanuel.salazar@avantica.net>
2022-04-25 20:59:43 -0600

    Adding Assert

--- SeleniumFirstProject/SimpleApplicationRunner.cs
+++ SeleniumFirstProject/SimpleApplicationRunner.cs
@@ -1,4 +1,5 @@
-﻿using OpenQA.Selenium;
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using WebDriverManager;
 using WebDriverManager.DriverConfigs.Impl;
@@ -14,6 +15,28 @@ namespace SelenmiumFirstProject
             IWebDriver driver = new ChromeDriver();
             driver.Manage().Window.Maximize();
             driver.Navigate().GoToUrl("https://github.com");
+
+            string searchPhrase = "selenium";
+
+            IWebElement searchInput = driver.FindElement(By.Name("q"));
+            searchInput.SendKeys(searchPhrase);
+            searchInput.SendKeys(Keys.Enter);
+
+            IList<String> actualItems = driver
+                .FindElements(By.ClassName("v-align-middle"))
+                .Where(xx => xx.TagName.Equals("a"))
+                .Select(xx => xx.Text.ToLower())
+                .ToList();
+
+            foreach(String item in actualItems)
+            {
+                Console.WriteLine(item);
+            }
+
+            Assert.True(actualItems.All(yy => yy.ToLower().Contains(searchPhrase)));
+
+            driver.Quit();
+
         }
     }
 }
